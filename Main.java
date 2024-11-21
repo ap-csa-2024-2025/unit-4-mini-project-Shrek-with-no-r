dafe7e45372d752ca5e027926efdd6dab8369f3a
Shrek-with-no-r <cmsarr100@student.hbuhsd.edu>
2024-11-21 19:20:42 +0000

    asd

--- Main.java
+++ Main.java
@@ -20,8 +20,21 @@ public class Main
 	// This method should implement Algorithm 1 and print all neccessary data
 	public static void algorithm1(String input)
 	{
-		// TODO: Put solution to Algorithm 1 here
-		System.out.println("Implement me!");
+		
+		for (int i = 0; i < input.length() - 1; i++)
+		{
+			String ABC = input.substring(i, i+1);
+
+			if (input.substring(i, i + 1).isUpperCase())
+			{
+				input.substring(i, i + 1).toLowerCase();
+			}
+			if (ABC.equals("a") || ABC.equals("e") || ABC.equals(i) || ABC.equals("o") || ABC.equals("u") || !ABC.equals(" a") || !ABC.equals(" e") || !ABC.equals(" i") || !ABC.equals(" o") || !ABC.equals(" u"))                                          
+			{
+				input = input.substring(0, i - input.length()) + (input.length() - 1);
+			}
+		}
+		System.out.println(input);
 	}
 
 	// This method should implement Algorithm 2 and print all neccessary data
